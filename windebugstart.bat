3014ab731fad5dc912e3d5c765e39ec92a051993
pinchyCZN <mail2@crazynation.org>
2013-12-04 13:32:19 -0600

    updates

--- windebugstart.bat
+++ windebugstart.bat
@@ -1,1 +1,5 @@
+if not exist E:\DEV\WinDDK\Debuggers\windbg.exe goto next
 start E:\DEV\WinDDK\Debuggers\windbg -b -k com:pipe,port=\\.\pipe\com_1,resets=0
+exit
+:next
+start "" "C:\Program Files\Debugging Tools for Windows (x86)\windbg" -b -k com:pipe,port=\\.\pipe\com_1,resets=0
